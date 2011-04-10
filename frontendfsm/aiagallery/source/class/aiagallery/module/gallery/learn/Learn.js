4f22055185d06b2b8c912e3d9b755a30bfc1ee4a
Derrell Lipman <derrell@dworkin.(none)>
2011-04-10 10:03:42 -0400

    New module files for gallery, containing a bit of example code

---
+++ frontendfsm/aiagallery/source/class/aiagallery/module/gallery/learn/Learn.js
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2011 Derrell Lipman
+ * 
+ * License:
+ *   LGPL: http://www.gnu.org/licenses/lgpl.html 
+ *   EPL : http://www.eclipse.org/org/documents/epl-v10.php
+ */
+
+/**
+ * Gallery "Learn" Page
+ */
+qx.Class.define("aiagallery.module.gallery.learn.Learn",
+{
+  type : "singleton",
+  extend : aiagallery.main.AbstractModule,
+
+  members :
+  {
+    /**
+     * Create the module's finite state machine and graphical user interface.
+     *
+     * This function is called the first time a module is actually selected to
+     * appear.  Creation of the module's actual FSM and GUI have been deferred
+     * until they were actually needed (now) so we need to create them.
+     *
+     * @param module {aiagallery.main.Module}
+     *   The module descriptor for the module.
+     */
+    initialAppear : function(module)
+    {
+      // Replace existing (temporary) finite state machine with the real one.
+      aiagallery.module.gallery.learn.Fsm.getInstance().buildFsm(module);
+
+      // Create the real gui.
+      aiagallery.module.gallery.learn.Gui.getInstance().buildGui(module);
+    }
+  }
+});
