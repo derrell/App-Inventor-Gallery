8a80dfe6c8e7c70b441aa464e551c62f037e8896
Derrell Lipman <derrell.lipman@unwireduniverse.com>
2011-07-25 10:38:25 -0400

    Add the missing mixin for returning login ingo

---
+++ frontendfsm/aiagallery/source/class/aiagallery/dbif/MWhoAmI.js
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2011 Derrell Lipman
+ * 
+ * License:
+ *   LGPL: http://www.gnu.org/licenses/lgpl.html 
+ *   EPL : http://www.eclipse.org/org/documents/epl-v10.php
+ */
+
+qx.Mixin.define("aiagallery.dbif.MWhoAmI",
+{
+  construct : function()
+  {
+    this.registerService("whoAmI", this.whoAmI);
+  },
+
+  members :
+  {
+    /**
+     * Return the user's current login information and, optionally a logout
+     * URL.
+     */
+    whoAmI : function()
+    {
+      var             ret;
+      var             me;
+      var             whoami;
+      
+      // Get the object indicating who we're logged in as
+      whoami = aiagallery.dbif.MDbifCommon.__whoami;
+      
+      // Obtain this dude's Visitor record
+      me = new aiagallery.dbif.ObjVisitors(whoami.email);
+      
+      // Create the return object, initialized to a clone of whoami. Add
+      // permissions from the database.
+      ret =
+        {
+          email       : String(whoami.email),
+          userId      : String(whoami.userId),
+          isAdmin     : whoami.isAdmin,
+          logoutUrl   : (qx.lang.Type.isArray(whoami.logoutUrl)
+                         ? whoami.logoutUrl
+                         : String(whoami.logoutUrl)),
+          permissions : me.getData().permissions.slice(0)
+        };
+
+      return ret;
+    }
+  }
+});
