4579b530dc1b5ea5209b5c9f752d739b98548a99
Reed Spool <reed_spool@student.uml.edu>
2011-07-28 18:15:10 -0400

    improving testing

--- frontendfsm/aiagallery/source/class/aiagallery/test/PermissionsNoneTest.js
+++ frontendfsm/aiagallery/source/class/aiagallery/test/PermissionsNoneTest.js
@@ -36,15 +36,19 @@ qx.Class.define("aiagallery.test.PermissionsNoneTest",
       apps:     {}
      
     },
+
+    // Instantiate this to aiagalelry.dbif.DbifSim.getInstance()
+    dbifSim : null,
     
     // This string is to be appended to assertions, to be specific about test conditions
     permissionLevel : "no permissions",
     
     setUp: function()
     {
-      // Start the RPC simulator by getting its singleton instance
-      this.dbif = aiagallery.dbif.DbifSim.getInstance();
-      
+      // Get access to the RPC implementations. This includes the mixins for
+      // all RPCs.      
+      this.dbifSim = aiagallery.dbif.DbifSim.getInstance();
+
       // Use a personalized database
       rpcjs.sim.Dbif.setDb(this.__db);
       
@@ -53,25 +57,110 @@ qx.Class.define("aiagallery.test.PermissionsNoneTest",
 
     tearDown: function() 
     {
-//      this.rpc.dispose();
     },
     
-    "test: attempt to addOrEditApp with no permissions" : function()
+    "test: attempt to getAppList" : function()
     {
-      
-      // Get access to the RPC implementations. This includes the mixins for
-      // all RPCs.
-      var dbifSim = aiagallery.dbif.DbifSim.getInstance();      
-           
+      // Must be logged in
+      this.assert(aiagallery.dbif.MDbifCommon.authenticate("aiagallery.features.getAppList"), "getAppList with " + this.permissionLevel);
+    },
+    
+    "test: attempt to addOrEditApp" : function()
+    {
+      // Must be logged in
       this.assert(aiagallery.dbif.MDbifCommon.authenticate("aiagallery.features.addOrEditApp"), "addOrEditApp with " + this.permissionLevel);
     },
-   
+    
+    "test: attempt to deleteApp" : function()
+    {
+      // Check Permissions
+      this.assertFalse(aiagallery.dbif.MDbifCommon.authenticate("aiagallery.features.deleteApp"), "deleteApp with " + this.permissionLevel);
+    },
+    
+    "test: attempt to getAppListAll" : function()
+    {
+      // FIXME: TEMPORARILY ANON, SHOULD BE CHECK PERMISSIONS
+      this.assert(aiagallery.dbif.MDbifCommon.authenticate("aiagallery.features.getAppListAll"), "getAppListAll with " + this.permissionLevel);
+    },
+    
+    "test: attempt to appQuery" : function()
+    {
+      // Anonymous
+      this.assert(aiagallery.dbif.MDbifCommon.authenticate("aiagallery.features.appQuery"), "appQuery with " + this.permissionLevel);
+    },
+
+    "test: attempt to getAppInfo" : function()
+    {
+      // Anonymous
+      this.assert(aiagallery.dbif.MDbifCommon.authenticate("aiagallery.features.getAppInfo"), "getAppInfo with " + this.permissionLevel);
+    },    
+    
+    "test: attempt to addComment" : function()
+    {
+      // Must be logged in
+      this.assert(aiagallery.dbif.MDbifCommon.authenticate("aiagallery.features.addComment"), "addComment with " + this.permissionLevel);
+    },
+    
+    "test: attempt to deleteComment" : function()
+    {
+      // Check Permissions
+      this.assertFalse(aiagallery.dbif.MDbifCommon.authenticate("aiagallery.features.deleteComment"), "deleteComment with " + this.permissionLevel);
+    },    
+
+    "test: attempt to getComments" : function()
+    {
+      // Anonymous
+      this.assert(aiagallery.dbif.MDbifCommon.authenticate("aiagallery.features.getComments"), "getComments with " + this.permissionLevel);
+    },
+
+    "test: attempt to mobileRequest" : function()
+    {
+      // Anonymous
+      this.assert(aiagallery.dbif.MDbifCommon.authenticate("aiagallery.features.mobileRequest"), "mobileRequest with " + this.permissionLevel);
+    },
+
+    "test: attempt to getCategoryTags" : function()
+    {
+      // Anonymous
+      this.assert(aiagallery.dbif.MDbifCommon.authenticate("aiagallery.features.getCategoryTags"), "getCategoryTags with " + this.permissionLevel);
+    },
+
+    "test: attempt to addOrEditVisitor" : function()
+    {
+      // Check Permissions
+      this.assertFalse(aiagallery.dbif.MDbifCommon.authenticate("aiagallery.features.addOrEditVisitor"), "addOrEditVisitor with " + this.permissionLevel);
+    },
+
+    "test: attempt to deleteVisitor" : function()
+    {
+      // Check permissions
+      this.assertFalse(aiagallery.dbif.MDbifCommon.authenticate("aiagallery.features.deleteVisitor"), "deleteVisitor with " + this.permissionLevel);
+    },
+    
+    "test: attempt to getVisitorList" : function()
+    {
+      // Check permissions
+      this.assertFalse(aiagallery.dbif.MDbifCommon.authenticate("aiagallery.features.getVisitorList"), "getVisitorList with " + this.permissionLevel);
+    },
+
+    "test: attempt to editProfile" : function()
+    {
+      // Must be logged in
+      this.assert(aiagallery.dbif.MDbifCommon.authenticate("aiagallery.features.editProfile"), "editProfile with " + this.permissionLevel);
+    },
+
+    "test: attempt to whoAmI" : function()
+    {
+      // Anonymous
+      this.assert(aiagallery.dbif.MDbifCommon.authenticate("aiagallery.features.whoAmI"), "whoAmI with " + this.permissionLevel);
+    },
+    
     "test: MDBifCommon._deepPermissionCheck()" : function()
     {
       
       // Get access to the RPC implementations. This includes the mixins for
       // all RPCs.
-      var dbifSim = aiagallery.dbif.DbifSim.getInstance();      
+      this.dbifSim;      
       
       
       
