7d296b4a9265de76faebad13c3acbaa8fd8c6750
Derrell Lipman <derrell.lipman@unwireduniverse.com>
2011-10-16 11:42:58 -0400

    Refactor to properly use refactored rpcjs
    Background:
    
    - Formerly, the rpcjs database code handled registration of RPC services, and
      there was an unnecessarily tight relationship between the two feature
      sets. That tight relationship has been removed. RPC services are registered
      in the AbstractRpcHandler class now (with convenience methods so the
      abstract one is not obviously accessed)
    
    - Formerly, RPC services were accessed via some given key, followed by the
      literal "features", followed by the method name. That has now been
      generalized, so that instead of passing a single key, an array is passed,
      which provides the entire path for each later-registered RPC.
    
    Changes here:
    
    - We now need pass no additional parameters to the superclass in DbifAppEngine
      and DbifSim. Those parameters were RPC-related. Instead, we now instantiate
      the appropriate Rpc object, and pass the array of RPC Keys to it.
    
    - Since each mixin to add RPCs is registering its services by calling
      this.registerService(), added a convenience function registerService() to
      DbifAppEngine and DbifSim that calls the instantiated RPC object's
      registerService() method.
    
    - Added a processRequest method that calls the instantiated RPC object's
      processRequest() method.
    
    Derrell

--- frontendfsm/aiagallery/source/class/aiagallery/dbif/DbifSim.js
+++ frontendfsm/aiagallery/source/class/aiagallery/dbif/DbifSim.js
@@ -20,7 +20,10 @@ qx.Class.define("aiagallery.dbif.DbifSim",
   construct : function()
   {
     // Call the superclass constructor
-    this.base(arguments, "aiagallery", "/rpc");
+    this.base(arguments);
+    
+    // Prepare for remote procedure calls to aiagallery.features.*
+    this.__rpc = new rpcjs.sim.Rpc([ "aiagallery", "features" ], "/rpc");
         
     if (true)
     {
@@ -43,6 +46,29 @@ qx.Class.define("aiagallery.dbif.DbifSim",
           isAdmin   : false,
           logoutUrl : "javascript:alert(\"logout\")"
         });
+    }
+  },
+  
+  members :
+  {
+    __rpc : null,
+
+    /**
+     * Register a service name and function.
+     *
+     * @param serviceName {String}
+     *   The name of this service within the <[rpcKey]> namespace.
+     *
+     * @param fService {Function}
+     *   The function which implements the given service name.
+     * 
+     * @param paramNames {Array}
+     *   The names of the formal parameters, in order.
+     */
+    registerService : function(serviceName, fService, paramNames)
+    {
+      // Register with the RPC provider
+      this.__rpc.registerService(serviceName, fService, this, paramNames);
     }
   },
   
