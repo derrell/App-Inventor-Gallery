3cbd78c2ca66a3b1b2f6f57b83dd28e51ed62ae0
Chris Adler <cadler42@gmail.com>
2011-10-27 18:57:27 -0400

    added MFlags.js with initial flagIt code to be tested

---
+++ frontendfsm/aiagallery/source/class/aiagallery/dbif/MFlags.js
@@ -0,0 +1,108 @@
+/**
+ * Copyright (c) 2011 Reed Spool
+ * 
+ * License:
+ *   LGPL: http://www.gnu.org/licenses/lgpl.html 
+ *   EPL : http://www.eclipse.org/org/documents/epl-v10.php
+ */
+
+qx.Mixin.define("aiagallery.dbif.MFlags",
+{
+  construct : function()
+  {
+
+    this.registerService("flagIt",
+                         this.flagIt,
+                         [ "appId" ]);
+  },
+
+  statics :
+  {
+
+  },
+
+  members :
+  {
+    /**
+     *  Add one to the number of times this app has been liked
+     * 
+     * @param appId {Integer}
+     *   This is either a string or number which is the uid of the app which is
+     *   being liked.
+     * 
+     * @return {Integer || Error}
+     *   This is the number of times this app has been liked, or an error if
+     *   the app was not found 
+     * 
+     */
+    flagIt : function(flagType, explanationInput, appId, commentId)
+    {
+      var            appObj;
+      var            appDataObj;
+      var            visitorId;
+      var            appNum;
+      var            result;
+      var            criteria;
+      var            newFlag;
+      var            Data;
+      var            maxFlags;
+      var            statusVals;
+      var            flagTypeVal;
+
+      maxFlags = aiagallery.dbif.Constants.MAX_FLAGGED;
+      statusVals = aiagallery.dbif.Constants.Status;
+      flagTypeVal = aiagallery.dbif.Constants.FlagType;
+
+      appObj = new aiagallery.dbif.ObjAppData(appId);
+      appDataObj = appObj.getData();
+      visitorId = this.whoAmI().email;
+      appNum = appDataObj.uid;
+
+      switch (flagType)
+      {
+        case flagTypeVal.app:
+
+          if (appObj.getBrandNew())
+          {
+            error.setCode(1);
+            error.setMessage(
+              "Comment with that ID not found. Unable to flag.");
+            return error;
+          }
+
+          newFlag = new aiagallery.dbif.ObjFlags();
+
+          var Data = {
+            type        : flagType,
+            app         : appNum,
+            comment     : null,
+            visitor     : visitorId,
+            explanation : explanationInput
+          }
+
+	  newFlag.setData(Data);
+          appDataObj.numCurFlags++;      
+
+          if(appDataObj.numCurFlags >= maxFlags)
+          {
+            appDataObj.status = statusVals.Pending;       
+            alert("email to be sent");
+          }
+
+          appObj.put();
+          newFlag.put();
+          break;
+
+        case flagTypeVal.comment:
+
+          break;
+        case default:
+      } 
+      return appDataObj.status;
+    }
+  }
+});
+
+
+
+
