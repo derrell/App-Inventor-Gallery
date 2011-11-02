b49b78f1830402b0fe423b0370031598ee4b0d7c
Terrance <Tscanausa@gmail.com>
2011-11-01 23:28:24 -0400

    Issue 71 this should fix the tab redirection.

--- frontendfsm/aiagallery/source/class/aiagallery/module/dgallery/appinfo/AppInfo.js
+++ frontendfsm/aiagallery/source/class/aiagallery/module/dgallery/appinfo/AppInfo.js
@@ -29,6 +29,7 @@ qx.Class.define("aiagallery.module.dgallery.appinfo.AppInfo",
       var             app;
       var             page;
       var             moduleList;
+      var             backtrack = {};
 
       // Get the main tab view
       var mainTabs = qx.core.Init.getApplication().getUserData("mainTabs");
@@ -51,6 +52,16 @@ qx.Class.define("aiagallery.module.dgallery.appinfo.AppInfo",
       // If we didn't find an existing tab, create a new one.
       if (! page)
       {
+        // Get the backtrack data
+        //backtrack = app.getUserdata("backtrack");
+        
+        // Set the new modules information to backtrack
+        //backtrack[label] = mainTabs.getSelection()[0];
+        backtrack = mainTabs.getSelection()[0];
+        
+        // Push backtrack into the userdata
+        //app.setUserData("backtrack", backtrack);
+          
         // Create a new module (tab) for this application
         app = new aiagallery.main.Module(
                 label,
@@ -68,7 +79,14 @@ qx.Class.define("aiagallery.module.dgallery.appinfo.AppInfo",
 
                     // Select the new application page
                     mainTabs.setSelection([ page ]);
-                  }
+                    
+                    // Addlistener for on close event
+                    page.addListener("close", function() 
+                                              {
+                                                mainTabs.setSelection([ backtrack ]);
+                                              }
+                                    );
+                 }
                 ],
                 true     // Instantiate a new module for each app
         );
